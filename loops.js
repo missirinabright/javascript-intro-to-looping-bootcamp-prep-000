6019e679388fde724f9bcbbe74e83f9a0096b8ba
Irina Bright <missirinabright@gmail.com>
2019-04-22 03:50:30 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,58 @@
+function forLoop(array) {
+for (let i = 0; i < 25; i++) {
+  if (i ===1) {
+    array.push(`I am ${i} strange loop.`);
+  } else {
+    array.push(`I am ${i} strange loops.`);
+}
+}
+return array;
+}
+
+function whileLoop(n) {
+  while (n > 0)
+{console.log(n--)}
+return 'done';
+}
+
+function doWhileLoop(num) {
+  do {
+  console.log("I run once regardless.");
+} while (Math.floor()*100 === 0);
+}
+
+function incrementVariable() {
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+}
+
+// describe('doWhileLoop(num)', () => {
+//     it ('console logs "I run once regardless." 1 time when passed an integer of 0 as a parameter.', () => {
+//         const spy = chai.spy.on(console, 'log');
+//         doWhileLoop(0);
+//         expect(spy).to.have.been.called.exactly(1);
+//     })
+
+//     it ('console logs "I run once regardless." 10 times when passed an integer of 10 as a parameter.', () => {
+//       const spy = chai.spy.on(console, 'log');
+//       doWhileLoop(10);
+//       expect(spy).to.have.been.called.exactly(10);
+//     })
+//   })
+// })
+
+// //   describe('doWhileLoop(num)', () => {
+// //     it ('console logs "I run once regardless." 1 time when passed an integer of 0 as a parameter.', () => {
+// //         const spy = chai.spy.on(console, 'log');
+// //         doWhileLoop(0);
+// //         expect(spy).to.have.been.called.exactly(1);
+// //     })
+
+// //     it ('console logs "I run once regardless." 10 times when passed an integer of 10 as a parameter.', () => {
+// //       const spy = chai.spy.on(console, 'log');
+// //       doWhileLoop(10);
+// //       expect(spy).to.have.been.called.exactly(10);
+// //     })
+// //   })
+// // })
